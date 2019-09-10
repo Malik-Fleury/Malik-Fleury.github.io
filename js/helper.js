8e1b70b6d9984b30b78c7d4f15ecaedd77618000
malik.fleury <malik.fleury@he-arc.ch>
2019-09-10 14:42:42 +0200

    Add skill pies

--- js/helper.js
+++ js/helper.js
@@ -1,69 +1,63 @@
 
-function createSkillCharts(divName)
+function createSkillCharts(divName, skillsDic)
 {
-  var skillsDic = {};
-
-  skillsDic['C#'] = 75;
-  skillsDic['Java'] = 75;
-  skillsDic['C++'] = 75;
-  skillsDic['Python'] = 75;
-  skillsDic['HTML'] = 100;
-  skillsDic['CSS'] = 50;
-  skillsDic['PHP'] = 50;
-  skillsDic['Javascript'] = 75;
-
   var skillsDiv = document.getElementById(divName);
 
   Object.keys(skillsDic).forEach(function(key) {
-    var newChartCanvas = document.createElement('canvas');
-    newChartCanvas.id = key;
-    var newSkillDiv = document.createElement('div');
-    newSkillDiv.className = "chart-skill";
-    newSkillDiv.appendChild(newChartCanvas);
+    var chartDiv = document.createElement('div');
+    chartDiv.className = 'chart';
+    chartDiv.setAttribute('data-percent', skillsDic[key]);
 
-    skillsDiv.appendChild(newSkillDiv);
+    var percentSpan = document.createElement('span');
+    percentSpan.className = 'percent';
+    percentSpan.innerHTML = skillsDic[key];
 
-    createChart(key, key, skillsDic[key]);
+    var chartTitle = document.createElement('h2');
+    chartTitle.innerHTML = key;
+
+    chartDiv.appendChild(percentSpan);
+    chartDiv.appendChild(chartTitle);
+    skillsDiv.appendChild(chartDiv);
   });
 }
 
-function createChart(divName, title, pourcent)
-{
-  var ctx = document.getElementById(divName).getContext('2d');
-  var chart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-      datasets: [{
-          data: [pourcent, 100 - pourcent],
-          backgroundColor: [
-              'rgba(17, 18, 18, 0.90)',
-              'rgba(255, 255, 255, 0.90)',
-          ],
-          borderColor: [
-              'rgba(17, 18, 18, 1.0)',
-              'rgba(17, 18, 18, 1.0)'
-          ],
-          borderWidth: 1
-      }]
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: true,
-      title: {
-          display: true,
-          text: title
-      },
-      scales: {
-          yAxes: [{
-              gridLines: {
-                  drawBorder: false,
-                  display: false,
-              },
-              ticks: {
-                  display: false
-              }
-          }]
-      }
-  }
-  });
-}
+// function createChart(divName, title, pourcent)
+// {
+//   var ctx = document.getElementById(divName).getContext('2d');
+//   var chart = new Chart(ctx, {
+//   type: 'pie',
+//   data: {
+//       datasets: [{
+//           data: [pourcent, 100 - pourcent],
+//           backgroundColor: [
+//               'rgba(17, 18, 18, 0.90)',
+//               'rgba(255, 255, 255, 0.90)',
+//           ],
+//           borderColor: [
+//               'rgba(17, 18, 18, 1.0)',
+//               'rgba(17, 18, 18, 1.0)'
+//           ],
+//           borderWidth: 1
+//       }]
+//   },
+//   options: {
+//     responsive: true,
+//     maintainAspectRatio: true,
+//       title: {
+//           display: true,
+//           text: title
+//       },
+//       scales: {
+//           yAxes: [{
+//               gridLines: {
+//                   drawBorder: false,
+//                   display: false,
+//               },
+//               ticks: {
+//                   display: false
+//               }
+//           }]
+//       }
+//   }
+//   });
+// }
--- js/scripts.js
+++ js/scripts.js
@@ -94,50 +94,41 @@
         });
     });
 
-    // Build charts
-    //createSkillCharts("skill-charts");
+    // Set skills
+    var skillsDic = {};
+    skillsDic['C#'] = 75;
+    skillsDic['Java'] = 75;
+    skillsDic['C++'] = 75;
+    skillsDic['Python'] = 75;
+    skillsDic['HTML'] = 100;
+    skillsDic['CSS'] = 50;
+    skillsDic['PHP'] = 50;
+    skillsDic['Javascript'] = 75;
+    skillsDic['WebGL1'] = 75;
 
-    // Gallery
-    var gallery = $('.gallery a').simpleLightbox();
-    gallery.on('show.simplelightbox', function(){
-      console.log('Requested for showing');
-    })
-    .on('shown.simplelightbox', function(){
-      console.log('Shown');
-    })
-    .on('close.simplelightbox', function(){
-      console.log('Requested for closing');
-    })
-    .on('closed.simplelightbox', function(){
-      console.log('Closed');
-    })
-    .on('change.simplelightbox', function(){
-      console.log('Requested for change');
-    })
-    .on('next.simplelightbox', function(){
-      console.log('Requested for next');
-    })
-    .on('prev.simplelightbox', function(){
-      console.log('Requested for prev');
-    })
-    .on('nextImageLoaded.simplelightbox', function(){
-      console.log('Next image loaded');
-    })
-    .on('prevImageLoaded.simplelightbox', function(){
-      console.log('Prev image loaded');
-    })
-    .on('changed.simplelightbox', function(){
-      console.log('Image changed');
-    })
-    .on('nextDone.simplelightbox', function(){
-      console.log('Image changed to next');
-    })
-    .on('prevDone.simplelightbox', function(){
-      console.log('Image changed to prev');
-    })
-    .on('error.simplelightbox', function(e){
-      console.log('No image found, go to the next/prev');
-      console.log(e);
+    // Build charts
+    createSkillCharts('skill-charts', skillsDic);
+
+    // Configure charts
+    $('.chart').easyPieChart({
+      // The color of the curcular bar. You can pass either a css valid color string like rgb, rgba hex or string colors. But you can also pass a function that accepts the current percentage as a value to return a dynamically generated color.
+      barColor: '#111212',
+      // The color of the track for the bar, false to disable rendering.
+      trackColor: '#f2f2f2',
+      // The color of the scale lines, false to disable rendering.
+      scaleColor: '#dfe0e0',
+      // Defines how the ending of the bar line looks like. Possible values are: butt, round and square.
+      lineCap: 'round',
+      // Width of the bar line in px.
+      lineWidth: 8,
+      // Size of the pie chart in px. It will always be a square.
+      size: 110,
+      // Time in milliseconds for a eased animation of the bar growing, or false to deactivate.
+      animate: 1500,
+      // Callback function that is called at the start of any animation (only if animate is not false).
+      onStart: $.noop,
+      // Callback function that is called at the end of any animation (only if animate is not false).
+      onStop: $.noop
     });
 
 })(jQuery);
